9e38bcde4768369bd061999dd43d2ef71d280d88
Gyuris Csaba <gyuris.csaba@vasvari.org>
2025-10-15 10:31:16 +0200

    Ha csak a property nevét írjuk át akkor a kódban máshol nem írodik át. Célszer  az IDE Rename funkcióját használni

--- Kreata.Backend/Context/ModelBuilderExtension.cs
+++ Kreata.Backend/Context/ModelBuilderExtension.cs
@@ -12,16 +12,16 @@ namespace MyApp.Backend.Context
 
             List<Student> students = new List<Student>
             {
-                new Student { Id = Guid.NewGuid(), FirstName = "Anna", LastName = "Kovács", BirthsDay = new DateTime(2007, 5, 12) },
-                new Student { Id = Guid.NewGuid(), FirstName = "Bence", LastName = "Szabó", BirthsDay = new DateTime(2006, 11, 3) },
-                new Student { Id = Guid.NewGuid(), FirstName = "Csilla", LastName = "Tóth", BirthsDay = new DateTime(2008, 1, 27) },
-                new Student { Id = Guid.NewGuid(), FirstName = "Dávid", LastName = "Varga", BirthsDay = new DateTime(2007, 8, 9) },
-                new Student { Id = Guid.NewGuid(), FirstName = "Eszter", LastName = "Nagy", BirthsDay = new DateTime(2006, 2, 14) },
-                new Student { Id = Guid.NewGuid(), FirstName = "Ferenc", LastName = "Kiss", BirthsDay = new DateTime(2008, 4, 30) },
-                new Student { Id = Guid.NewGuid(), FirstName = "Gábor", LastName = "Molnár", BirthsDay = new DateTime(2007, 10, 21) },
-                new Student { Id = Guid.NewGuid(), FirstName = "Hanna", LastName = "Horváth", BirthsDay = new DateTime(2006, 7, 18) },
-                new Student { Id = Guid.NewGuid(), FirstName = "István", LastName = "Balogh", BirthsDay = new DateTime(2007, 12, 5) },
-                new Student { Id = Guid.NewGuid(), FirstName = "Júlia", LastName = "Farkas", BirthsDay = new DateTime(2008, 3, 22) }
+                new Student { Id = Guid.NewGuid(), FirstName = "Anna", LastName = "Kovács", BirthDate = new DateTime(2007, 5, 12) },
+                new Student { Id = Guid.NewGuid(), FirstName = "Bence", LastName = "Szabó", BirthDate = new DateTime(2006, 11, 3) },
+                new Student { Id = Guid.NewGuid(), FirstName = "Csilla", LastName = "Tóth", BirthDate = new DateTime(2008, 1, 27) },
+                new Student { Id = Guid.NewGuid(), FirstName = "Dávid", LastName = "Varga", BirthDate = new DateTime(2007, 8, 9) },
+                new Student { Id = Guid.NewGuid(), FirstName = "Eszter", LastName = "Nagy", BirthDate = new DateTime(2006, 2, 14) },
+                new Student { Id = Guid.NewGuid(), FirstName = "Ferenc", LastName = "Kiss", BirthDate = new DateTime(2008, 4, 30) },
+                new Student { Id = Guid.NewGuid(), FirstName = "Gábor", LastName = "Molnár", BirthDate = new DateTime(2007, 10, 21) },
+                new Student { Id = Guid.NewGuid(), FirstName = "Hanna", LastName = "Horváth", BirthDate = new DateTime(2006, 7, 18) },
+                new Student { Id = Guid.NewGuid(), FirstName = "István", LastName = "Balogh", BirthDate = new DateTime(2007, 12, 5) },
+                new Student { Id = Guid.NewGuid(), FirstName = "Júlia", LastName = "Farkas", BirthDate = new DateTime(2008, 3, 22) }
             };
 
 
